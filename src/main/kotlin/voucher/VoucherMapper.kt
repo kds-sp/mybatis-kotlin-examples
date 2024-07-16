a3cba733a1ea53fef4121adab4aa143d7e0455d2
Kim Dong Seok <kds@soundplatform.co.kr>
2024-07-16 11:57:47 +0900

    클래슨 주석 추가

--- src/main/kotlin/voucher/VoucherMapper.kt
+++ src/main/kotlin/voucher/VoucherMapper.kt
@@ -2,6 +2,9 @@ package voucher
 
 import org.apache.ibatis.annotations.Select
 
+/**
+ * 인수 개수가 컬럼의 개수와 동일한 생성자를 Mybatis가 자동으로 선택함
+ */
 interface VoucherMapper {
     @Select(
 //        "select id, created_at, updated_at, applicable_plan_types, deactivate_reason, end_date_time, start_date_time, issued_count, registered_count, remain_count, is_activated, memo, name, prefix_code, is_expired",
@@ -16,9 +19,6 @@ interface VoucherMapper {
     )
     fun selectPromotionById아이디만(id: Int): PromotionDto아이디만
 
-    /**
-     * 인수 개수가 컬럼의 개수와 동일한 생성자를 Mybatis가 자동으로 선택함
-     */
     @Select(
         "select id, name, applicable_plan_types as 임의의컬럼명",
         "from promotion where id = #{value}"
