f6270e5534387de27e8c916b24025e9b19441118
Kim Dong Seok <kds@soundplatform.co.kr>
2024-07-16 09:34:25 +0900

    swagger 제거

--- src/main/kotlin/voucher/TermDTO.kt
+++ src/main/kotlin/voucher/TermDTO.kt
@@ -1,11 +1,6 @@
 package voucher
 
-import io.swagger.v3.oas.annotations.media.Schema
-
-@Schema(description = "바우처 윺효기간을 가진 객체 구조")
 data class TermDto(
-    @Schema(description = "프로모션 유효기간의 시작일", example = "2024-03-01 00:00:00")
     val expirationStartDate: String,
-    @Schema(description = "프로모션 유효기간의 종료일", example = "2024-03-30 23:59:59")
     val expirationEndDate: String,
 )
